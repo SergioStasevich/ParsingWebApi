8cf4d73a5fd3bc6de253841abeb53fdf5f72d277
SergioStasevich <kangurek1991@gmail.com>
2022-10-22 20:33:56 +0200

    API Pars json

--- ParsingWebApi/ViewController.swift
+++ ParsingWebApi/ViewController.swift
@@ -1,19 +1,37 @@
-//
-//  ViewController.swift
-//  ParsingWebApi
-//
-//  Created by Siarhei Stasevich on 22/10/2022.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labeljoke: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+    @IBAction func newJokeButton(_ sender: UIButton) {
+        makaRequest()
+        
+    }
+    private func makaRequest(){
+       var request = URLRequest(url: URL(string: "https://v2.jokeapi.dev/joke/Any")!)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data, let joke = try? JSONDecoder().decode(Joke.self, from: data) {
+                DispatchQueue.main.async {
+                    self.labeljoke.text = joke.setup
+                    print(joke.flags.racist)
+                }
+               
+            }
+            
+    }
+        task.resume()
+    }
+    
 
 }
 
